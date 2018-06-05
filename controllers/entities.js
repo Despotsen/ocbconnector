cc06d35ca33d24abbba12f3f716605bd98098a8c
Igor <igor.despot@eng.rs>
2018-06-05 11:41:02 +0200

    Add file check

--- controllers/entities.js
+++ controllers/entities.js
@@ -1,5 +1,7 @@
-const { entities } = require('../services/');
+const { entities, upload } = require('../services/');
 const messages = require('../utils/message.json');
+const config = require('../config');
+const path = require('path');
 
 function getEntities(req, res, id) {
   entities.processEntities(id)
@@ -12,8 +14,12 @@ function getEntities(req, res, id) {
     });
 }
 
-function postEntities(req, res, next) {
-  res.json('Hello from post');
+function postEntities(req, res, file) {
+  fileCheck(file[0], (err) => {
+    if (err) {
+      entitiesOperation.errorHandle(res, err);
+    }
+  });
 }
 
 const entitiesOperation = {
@@ -30,6 +36,19 @@ function codeChecker(code) {
     return messages['500'];
   }
   return messages[code];
+}
+
+function fileCheck(file, cb) {
+  allowedExt = config.ext;
+
+  if (!file) {
+    return cb('400')
+  }
+
+  if (!allowedExt.includes(path.extname(file.originalname))) {
+    return cb('400')
+  }
+  
 }
 
 module.exports = {
