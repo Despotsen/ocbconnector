cdc30bc933262b8849ba01d6f7be06fb650c07c2
Igor <igor.despot@eng.rs>
2018-06-06 18:15:11 +0200

    Add rules

---
+++ rules/Waste.js
@@ -0,0 +1,13 @@
+
+const Waste = {
+  id: String,
+  family: String,
+  type: String,
+  name: String,
+  description: String,
+  refCategory: String,
+  definitionSource: String,
+  wasteCode: String
+}
+
+module.exports = Waste;
---
+++ rules/index.js
@@ -0,0 +1,5 @@
+const Waste = require('./Waste');
+
+module.exports = {
+  Waste
+};
