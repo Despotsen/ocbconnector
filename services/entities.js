ec2b921643e9ef02ba9891b2b2e22272098e68ef
Igor <igor.despot@eng.rs>
2018-06-08 14:43:03 +0200

    Refactor entities

--- services/entities.js
+++ services/entities.js
@@ -1,7 +1,55 @@
 const request = require('request-promise');
 const url = require('../config').orion_url;
 
-function processEntities(id,headers) {
+const entitiesOperations = {
+
+  getEntities: headers => request({
+    method: 'GET',
+    headers: {
+      'Fiware-Service': headers['fiware-service'],
+      'Fiware-ServicePath': headers['fiware-servicepath']
+    },
+    uri: url,
+    json: true
+  }),
+
+  getEntity: (id, headers) => request({
+    method: 'GET',
+    headers: {
+      'Fiware-Service': headers['fiware-service'],
+      'Fiware-ServicePath': headers['fiware-servicepath']
+    },
+    uri: `${url}/${id}`,
+    json: true
+  }),
+
+  createEntity: (data, headers) => request({
+    method: 'POST',
+    headers: {
+      'Fiware-Service': headers['fiware-service'],
+      'Fiware-ServicePath': headers['fiware-servicepath']
+    },
+    uri: `${url}?options=keyValues`,
+    body: data,
+    json: true
+  }),
+
+  updateEntity: (data, headers) => request({
+    method: 'POST',
+    headers: {
+      'Fiware-Service': headers['fiware-service'],
+      'Fiware-ServicePath': headers['fiware-servicepath']
+    },
+    uri: `${url}/${data.id}/attrs?options=keyValues`,
+    body: Object.assign({}, data, {
+      id: undefined,
+      type: undefined
+    }),
+    json: true
+  })
+};
+
+function processEntities(id, headers) {
   if (!id) {
     return entitiesOperations.getEntities(headers);
   }
@@ -9,62 +57,11 @@ function processEntities(id,headers) {
 }
 
 function sendEntities(data, headers, operation) {
-  if(!operation) {
+  if (!operation) {
     return entitiesOperations.createEntity(data, headers);
   }
-  return entitiesOperations
+  return entitiesOperations;
 }
-
-const entitiesOperations = {
-
-  getEntities: (headers) => {
-    return request({
-      method: 'GET',
-      headers: {
-        "Fiware-Service": headers['fiware-service'],
-        "Fiware-ServicePath": headers['fiware-servicepath']
-      },
-      uri: url,
-      json: true
-    });
-  },
-
-  getEntity: (id, headers) => {
-    return request({
-      method: 'GET',
-      headers: {
-        "Fiware-Service": headers['fiware-service'],
-        "Fiware-ServicePath": headers['fiware-servicepath']
-      },
-      uri: url + '/' + id,
-      json: true
-    });
-  },
-
-  createEntity: (data,headers) => {
-    return request({
-      method: 'POST',
-      headers: {
-        "Fiware-Service": headers['fiware-service'],
-        "Fiware-ServicePath": headers['fiware-servicepath']
-      },
-      uri: `${url}/${data.id}/attrs?options=keyValues`,
-      body: Object.assign({}, data, {
-        id: undefined,
-        type: undefined
-      }),
-      json:true
-    });
-  },
-
-  updateEntity: (data, headers) => {
-    return request({
-
-    });
-  }
-};
-
-
 
 module.exports = {
   processEntities
