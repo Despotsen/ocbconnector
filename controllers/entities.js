b2c2ca5e50919978054e1b7fa7b6ab1ed705ecd7
Igor <igor.despot@eng.rs>
2018-06-08 14:47:41 +0200

    Refactor entities controller

--- controllers/entities.js
+++ controllers/entities.js
@@ -1,31 +1,35 @@
-const { entities, codeCheck, fileCheck, headersCheck, parser } = require('../services/');
-const utils = require('../utilities/utils.json');
-const path = require('path');
+const {
+  entities, codeCheck, fileCheck, headersCheck, parser
+} = require('../services/');
+
+const entitiesOperation = {
+  errorHandle: (res, code) => {
+    res.status(code)
+      .end(codeCheck);
+  }
+};
 
 function getEntities(req, res, id, headers) {
-
   headersCheck(headers, (err) => {
-    if(err) {
-    entitiesOperation.errorHandle(res, err);
+    if (err) {
+      entitiesOperation.errorHandle(res, err);
     }
   });
 
   entities.processEntities(id, headers)
-  .then((result) => {
-    res.status(200).json(result);
-  })
-  .catch((error) => {
-    entitiesOperation
-      .errorHandle(res, error.statusCode || 500);
-  });
-
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((error) => {
+      entitiesOperation
+        .errorHandle(res, error.statusCode || 500);
+    });
 }
 
 function postEntities(req, res, file, headers) {
-
   headersCheck(headers, (err) => {
-    if(err) {
-    entitiesOperation.errorHandle(res, err);
+    if (err) {
+      entitiesOperation.errorHandle(res, err);
     }
   });
 
@@ -38,20 +42,10 @@ function postEntities(req, res, file, headers) {
   parser.parse(file.buffer.toString()).then((result) => {
     res.json(result);
   })
-  .catch((error) => {
-    res.json(error)
-  });
-
+    .catch((error) => {
+      res.json(error);
+    });
 }
-
-const entitiesOperation = { 
-
-  errorHandle: (res, code) => {
-    res.status(code)
-      .end(JSON.stringify(codeCheck(code)), 'binary');
-  }
-
-};
 
 module.exports = {
   getEntities,
