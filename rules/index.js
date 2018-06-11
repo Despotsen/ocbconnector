bd1005646cb081f51b142ef8173c3e935e6eaf32
Igor <igor.despot@eng.rs>
2018-06-11 12:26:51 +0200

    Add rules export

--- rules/index.js
+++ rules/index.js
@@ -1,5 +1,21 @@
 const Waste = require('./Waste');
+const WasteCategory = require('./WasteCategory');
+const DepositPoint = require('./DepositPoint');
+const DepositPointType = require('./DepositPointType');
+const DepositPointIsle = require('./DepositPointIsle');
+const SortingType  = require('./SortingType');
+const Transaction = require('./Transaction');
+const Vehicle = require('./Vehicle');
+const VehicleType = require('./VehicleType');
 
 module.exports = {
-  Waste
+  Waste,
+  DepositPoint,
+  WasteCategory,
+  DepositPointType,
+  DepositPointIsle,
+  SortingType,
+  Transaction,
+  Vehicle,
+  VehicleType
 };
