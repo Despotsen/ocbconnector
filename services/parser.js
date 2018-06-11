83be6c5ac4d05838f56ad599cdf0154646a3eb7d
Igor <igor.despot@eng.rs>
2018-06-11 12:15:24 +0200

    Refactor code and get entity

--- services/parser.js
+++ services/parser.js
@@ -1,57 +1,51 @@
-const csv=require('csvtojson');
-const entityRules = require('../rules');
+const csv = require('csvtojson');
+const ruleChecks = require('../services/property').rulesCheck;
+const propertyChecks = require('./property').propertyChecks;
+const processEntity = require('./property').processEntity;
 
-function parse(rawData) {
-  return parseOperations.getData(rawData).then((parsedData) => {
-    return parseOperations.getRules(parsedData).then((rules) => {
-      return parseOperations.checkAttributes(rules,parsedData).then((attr) => {
-        return Promise.resolve(attr)
-      });
-    });
-  })
-  .catch((error) => {
-    return Promise.reject(error);
-  })
+function parse(rawData, option) {
+  return parseOperations.getData(rawData)
+    .then((parsedData) => {
+      return parseOperations.getRules(parsedData)
+        .then((rules) => {
+          return parseOperations.getEntity(rules, parsedData, option)
+            .then((checkedData) => {
+              return Promise.resolve(checkedData);
+            });
+        });
+    })
+    .catch((error) => Promise.reject(error));
 }
 
 const parseOperations = {
 
-  getData: (rawData) => {
-    return csv({delimiter: ';'}).fromString(rawData);
-  },
+  getData: (rawData) => csv({delimiter: ';'}).fromString(rawData),
 
   getRules: (parsedData) => {
-    const type = parsedData[0].type;
-    const rules = entityRules[type];
-    const errors = [];
-
-    if(!type) {
-      return Promise.reject('Failed to find type on ' + parsedData[0].id);
-    } else {
-      parsedData.forEach(element => {
-        if(!element.type || element.type !== type) {
-          errors.push(element.id);
-        }
-      });
+    let rules;
+    try {
+      rules = ruleChecks(parsedData);
+    } catch (error) {
+      return Promise.reject(error.message);
     }
-
-    if(errors.length != 0)
-      return Promise.reject('Invalid type attribute on: ' + errors);
-
-    if(!rules)
-      return Promise.reject('No rules have been found for: ' + type);
-
     return Promise.resolve(rules);
   },
 
-  checkAttributes: (rules, parsedData, options) => {
-    const result = {};
-    const properties = Object.getOwnPropertyNames(rules);
-    
-    return Promise.resolve(properties);
-  }
-}
+  getEntity: (rules, parsedData, option) => {
+    const result = [];
+    const errors = [];
+
+    parsedData.forEach((entity) => {
+     try {
+      result.push(processEntity(rules, entity));
+     } catch (error) {
+       errors.push(error.message);
+     }
+    });
+    return Promise.resolve({errors, result});
+  },
+};
 
 module.exports = {
-  parse
-}
+  parse,
+};
