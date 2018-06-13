50a030868f488bf44c7d8af9e3815d55a7e3b542
Igor <igor.despot@eng.rs>
2018-06-13 16:44:38 +0200

    Update service

--- services/entities.js
+++ services/entities.js
@@ -9,7 +9,7 @@ const entitiesOperations = {
       'Fiware-Service': headers['fiware-service'],
       'Fiware-ServicePath': headers['fiware-servicepath'],
     },
-    uri: url,
+    uri: `${url}entities/?limit=100`,
     json: true,
   }),
 
@@ -19,32 +19,37 @@ const entitiesOperations = {
       'Fiware-Service': headers['fiware-service'],
       'Fiware-ServicePath': headers['fiware-servicepath'],
     },
-    uri: `${url}/${id}`,
+    uri: `${url}entities/${id}`,
     json: true,
   }),
 
   createEntity: (data, headers) => request({
     method: 'POST',
     headers: {
+      'Content-Type': 'application/json',
       'Fiware-Service': headers['fiware-service'],
       'Fiware-ServicePath': headers['fiware-servicepath'],
     },
-    uri: `${url}?options=keyValues`,
-    body: data,
-    json: true,
+    uri: `${url}op/update?options=keyValues`,
+    body: {
+      actionType: 'APPEND',
+      entities: data
+    },
+    json: true
   }),
 
   updateEntity: (data, headers) => request({
     method: 'POST',
     headers: {
+      'Content-Type': 'application/json',
       'Fiware-Service': headers['fiware-service'],
       'Fiware-ServicePath': headers['fiware-servicepath'],
     },
-    uri: `${url}/${data.id}/attrs?options=keyValues`,
-    body: Object.assign({}, data, {
-      id: undefined,
-      type: undefined,
-    }),
+    uri: `${url}op/update?options=keyValues`,
+    body: {
+      actionType: 'UPDATE',
+      entities: data,
+    },
     json: true,
   }),
 };
@@ -56,13 +61,61 @@ function processEntities(id, headers) {
   return entitiesOperations.getEntity(id, headers);
 }
 
-function sendEntities(data, headers, operation) {
-  if (!operation) {
-    return entitiesOperations.createEntity(data, headers);
+async function sendEntities(data, headers, operation) {
+  // const few = chunkArray(data, 10);
+  // const ps = [];
+  // if (!operation) {
+  //   if (data.length <= 1000) {
+  //     return entitiesOperations.createEntity(data, headers);
+  //   }
+  //   for(let i = 0; i<1; i++) {
+  //     console.log('Sending: ' + i, few[i].length)
+  //     ps.push(await entitiesOperations.createEntity(few[i], headers));
+  //     console.log('Finished: ' + i, few[i].length)
+  //   }
+  //   Promise.all(ps)
+  //   .then((results) => {
+  //     console.log(results)
+  //       return Promise.resolve('good')
+  //   })
+  //   .catch((err) => {
+  //     console.log(err)
+  //     return Promise.reject(err);
+  //   })
+  // }
+  // return entitiesOperations.updateEntity(data, headers);
+
+  var slicer = 0;
+  var allBatches = []
+  while (data.length > slicer) {
+    let anchor = slicer;
+    slicer += 100;
+    allBatches.push(
+      entitiesOperations.createEntity(data.slice(anchor,slicer), headers)
+    );
+    console.log(allBatches.length-1);
+    allBatches[allBatches.length-1]
   }
-  return entitiesOperations;
+  return Promise.all(allBatches)
+    .then((x) => {return Promise.resolve('perfect yes money')})
+    .catch((e)=>{console.log(e)})
+}
+
+function chunkArray(myArray, chunk_size){
+  var index = 0;
+  var arrayLength = myArray.length;
+  var tempArray = [];;
+  
+  for (index = 0; index < arrayLength; index += chunk_size) {
+      myChunk = myArray.slice(index, index+chunk_size);
+      tempArray.push(myChunk);
+  }
+
+   return tempArray;
 }
 
 module.exports = {
   processEntities,
+  sendEntities,
+  entitiesOperations
 };
