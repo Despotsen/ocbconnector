9902372ef2d2aeda026dce334e5272a7ca254e84
Igor <igor.despot@eng.rs>
2018-06-17 12:55:10 +0200

    Add https support

--- config.js
+++ config.js
@@ -2,8 +2,10 @@ const config = {};
 
 config.orion_url = 'http://localhost:1026/v2/';
 
-config.api_url = '3000';
+config.api_port = '3000';
 
 config.ext = ['.csv'];
+
+config.https = true;
 
 module.exports = config;
