6cb18ce4ee6266b0e0e938feec4af6582e81c435
Igor <igor.despot@eng.rs>
2018-06-17 12:56:00 +0200

    Add rules

--- routes/entities.js
+++ routes/entities.js
@@ -3,6 +3,8 @@ const express = require('express');
 const router = express.Router();
 const { entities } = require('../controllers/');
 const { upload } = require('../middlewares');
+const rules = require('../utilities/rules.json');
+
 
 module.exports = () => {
   router.route('/v1/entities')
@@ -21,9 +23,29 @@ module.exports = () => {
       entities.getEntities(req, res, req.params.id, req.headers);
     });
 
-  router.route('/v1/rules/:id')
+  router.route('/v1/rules')
     .get((req, res) => {
-      res.json(`Rules: ${req.params.id}`);
+      res.json(rules['rules']);
+    });
+
+  router.route('/v1/rules/:id')
+  .get((req, res) => {
+    if(!rules[req.params.id]) {
+      res.status(404).json(`Rules details not found/exist for ${req.params.id}`);
+    }
+    res.status(200).json(rules[req.params.id]);
+  });
+
+  router.route('/v1/entities/type/:id')
+    .get((req, res) => {
+      entities.getEntitiesType(res, req.params.id, req.headers);
+    });
+
+  router.route('/v1/entities/update')
+  .post(
+    upload.multer,
+    (req, res) => {
+      entities.updateEntities(req, res, req.files[0], req.headers);
     });
 
   return router;
