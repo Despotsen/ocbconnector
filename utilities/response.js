69d4ece69a6c3174edc6958a82b7cff895c00b8f
Igor <igor.despot@eng.rs>
2018-06-18 15:41:41 +0200

    Add response payload

--- utilities/response.js
+++ utilities/response.js
@@ -2,3 +2,23 @@ module.exports = (x, y) => ({
   type: 'geo:point',
   coordinates: [x, y],
 });
+
+module.exports.resposneResult = (result, action) => {
+  const data = [];
+  result.forEach(element => {
+    data.push({
+      status: [{
+        type: element.type ? element.type : 'Unknown',
+        description: {
+          id: element.id ? element.id : 'Unknown'
+        },
+        actions: [{
+          error: '',
+          status: 'SUCCESS',
+          type: action
+        }]
+      }]
+    })
+  });
+  return data;
+}
