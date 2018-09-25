8a31486ed6aa5e1bfe1d158f0efcff251aa15e21
Igor <igor.despot@eng.rs>
2018-09-25 12:07:33 +0200

    Update config

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const config = {};
 
-config.orion_url = 'http://localhost/';
+config.orion_url = 'http://192.168.229.62:82';
 
-config.api_port = '3001';
+config.api_port = '3000';
 
 config.ext = ['.csv'];
 
