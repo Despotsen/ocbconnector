44ebccf2e50316fbc796658d2b3e78e47ed6c1d6
Igor Despot <igordesspot@gmail.com>
2018-11-26 03:25:05 -0800

    Add object destruction to export of rules

--- utilities/index.js
+++ utilities/index.js
@@ -1,26 +1,8 @@
-const mandatoryCheck = require('./rules').mandatoryCheck;
-const locationCheck = require('./rules').locationCheck;
-const commaNumToUnits = require('./rules').commaNumToUnits;
-const stringToArray = require('./rules').stringToArray;
-const dateCheck = require('./rules').dateCheck;
-const extraCheck = require('./rules').extraCheck;
-const maxCargoVolume = require('./rules').maxCargoVolume;
-const stringToArrayMandatory = require('./rules').stringToArrayMandatory;
-const commaNumToUnitsMandatory = require('./rules').commaNumToUnitsMandatory;
-const removeForbiden = require('./rules').removeForbiden;
-const removeForbidenStrict = require('./rules').removeForbidenStrict;
-const locationCheckNoMand = require('./rules').locationCheckNoMand;
-const stringCheck = require('./rules').idTypeCheck;
-const test = require('./rules').testCheck;
-const id = require('./rules').idspec;
-const stringToArrayNum = require('./rules').stringToArrayNum;
-
-module.exports = {
-  mandatoryCheck,
-  locationCheck,
+module.exports =  { 
+  mandatoryCheck, 
+  locationCheck, 
   commaNumToUnits,
   stringToArray,
-  dateCheck,
   dateCheck,
   extraCheck,
   maxCargoVolume,
@@ -29,8 +11,8 @@ module.exports = {
   removeForbiden,
   removeForbidenStrict,
   locationCheckNoMand,
-  stringCheck,
-  test,
-  id,
+  idTypeCheck,
+  testCheck,
+  idspec,
   stringToArrayNum
-};
+} = require("./rules");
