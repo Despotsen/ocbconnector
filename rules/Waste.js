7c9361fa222fc5dbc945b1d26dfde0e4e420daf3
Igor <igor.despot@eng.rs>
2018-12-21 11:11:50 +0100

    Update rules with new locale changes

--- rules/Waste.js
+++ rules/Waste.js
@@ -1,14 +1,26 @@
 const rules = require( "../utilities" );
 
 const Waste = {
-    id: rules.idCheck,
-    family: rules.mandatoryCheck,
-    type: rules.typeCheck,
-    name: rules.mandatoryCheck,
-    description: rules.stringCheck,
-    refCategory: rules.mandatoryCheck,
-    definitionSource: rules.stringCheck,
-    wasteCode: rules.stringCheck,
+    "id": rules.idCheck,
+    "type": rules.typeCheck,
+    "family": rules.mandatoryCheck,
+    "name": rules.mandatoryCheck,
+    "name:en": rules.stringCheck,
+    "name:es": rules.stringCheck,
+    "name:it": rules.stringCheck,
+    "name:pt": rules.stringCheck,
+    "name:eus": rules.stringCheck,
+    "name:gr": rules.stringCheck,
+    "description": rules.stringCheck,
+    "description:en": rules.stringCheck,
+    "description:es": rules.stringCheck,
+    "description:it": rules.stringCheck,
+    "description:pt": rules.stringCheck,
+    "description:eus": rules.stringCheck,
+    "description:gr": rules.stringCheck,
+    "refCategory": rules.mandatoryCheck,
+    "definitionSource": rules.stringCheck,
+    "wasteCode": rules.stringCheck
 };
 
 module.exports = Waste;
--- rules/WasteCategory.js
+++ rules/WasteCategory.js
@@ -1,11 +1,18 @@
 const rules = require( "../utilities" );
 
 const WasteCategory = {
-    id: rules.idCheck,
-    family: rules.mandatoryCheck,
-    type: rules.typeCheck,
-    name: rules.mandatoryCheck,
-    description: rules.stringCheck,
+    "id": rules.idCheck,
+    "type": rules.typeCheck,
+    "family": rules.mandatoryCheck,
+    "name": rules.mandatoryCheck,
+    "refResources": rules.stringCheck,
+    "description": rules.mandatoryCheck,
+    "description:en": rules.stringCheck,
+    "description:es": rules.stringCheck,
+    "description:it": rules.stringCheck,
+    "description:pt": rules.stringCheck,
+    "description:eus": rules.stringCheck,
+    "description:gr": rules.stringCheck,
 };
 
 module.exports = WasteCategory;
