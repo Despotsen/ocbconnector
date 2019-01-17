e9be42bf5898df6f0b38f911c67ef52dc67299d8
Igor <igor.despot@eng.rs>
2019-01-17 09:26:36 +0100

    Add fix to Fiware IDM idle problems

--- app.js
+++ app.js
@@ -19,6 +19,36 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", entities);
 
+setInterval(function() {
+  request({
+      method: "GET",
+      headers: {
+          "Fiware-Service": "waste4think",
+          "Fiware-ServicePath": "/deusto/w4t/cascais/real",
+          "X-Auth-Token": "DevelopmentTest",
+      },
+      uri: "http://backend.waste4think.eu/connector/entities",
+      json: true
+  })
+  .then(res => console.log(res))
+  .catch(err => console.log("Sent request to Ngsi Connector API"))
+}, 5 * 60 * 1000)
+
+setInterval(function() {
+  request({
+      method: "GET",
+      headers: {
+          "Fiware-Service": "waste4think",
+          "Fiware-ServicePath": "/deusto/w4t/cascais/real",
+          "X-Auth-Token": "DevelopmentTest",
+      },
+      uri: "http://backend.waste4think.eu/connector/test/entities",
+      json: true
+  })
+  .then(res => console.log(res))
+  .catch(err => console.log("Sent request to Test Ngsi Connector API"))
+}, 5 * 60 * 1000)
+
 app.use((req, res, next) => {
   const error = new Error("Requested route not found, more information on available routes can be found at: http://backend.waste4think.eu:81/");
   error.status = 404;
