a2381555c0ca9ad3792a69ae39f69818a6f592ac
Igor Despot <igordesspot@gmail.com>
2019-02-12 15:24:39 +0100

    Update config

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const config = {};
 
-config.orion_url = "http://192.168.229.62:88/";
+config.orion_url = "http://192.168.229.62:82/";
 
-config.api_port = "3002";
+config.api_port = "3000";
 
 config.ext = [ ".csv", ".json" ];
 
