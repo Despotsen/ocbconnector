dd57cb8fe940d6b48b26d329cffb8cb6509a2784
Igor <igor.despot@eng.rs>
2019-07-01 09:48:19 +0200

    Update max file size

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -3,7 +3,7 @@ const Multer = require("multer");
 const multer = Multer({
   storage: Multer.memoryStoragem,
   limits: {
-    fileSize: 5 * 1024 * 1024,
+    fileSize: 30 * 1024 * 1024,
   },
 }).any();
 
